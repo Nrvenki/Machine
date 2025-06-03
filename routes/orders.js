156c23ad0e467af172499ab60ca6d81034a9f3de
Nrvenki <huntergamind2kkids@gmail.com>
2025-06-03 11:26:01 +0530

    Your commit message

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
+const Order = require('../models/Order');
 const Machine = require('../models/Machine');
 
 // Order Schema
