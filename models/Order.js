9873ecd23edfcd1f67d41ba95c865bb4ff200e28
Nrvenki <huntergamind2kkids@gmail.com>
2025-06-03 11:08:54 +0530

    Your commit message

--- models/Order.js
+++ models/Order.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
   clientName: { type: String, required: true },
+  mobileNumber: { type: String, required: true }, // Add mobileNumber field
   machineId: { type: String, required: true },
   machineName: { type: String, required: true },
   quantity: { type: Number, required: true },
